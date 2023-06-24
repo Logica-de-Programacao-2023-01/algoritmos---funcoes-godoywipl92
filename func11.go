e08fff42d6f297afc2da29016dd1d3798d24dd1a
godoywipl92 <126252780+godoywipl92@users.noreply.github.com>
2023-06-24 13:18:52 -0300

    solução 11

--- func11.go
+++ func11.go
@@ -1,1 +1,40 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
+func main() {
+	numero := 17
+
+	primo, err := verificarNumeroPrimo(numero)
+
+	if err != nil {
+		fmt.Println("Erro:", err)
+	} else {
+		fmt.Println("É primo:", primo)
+	}
+}
+
+func verificarNumeroPrimo(num int) (bool, error) {
+	if num < 0 {
+		return false, errors.New("Número negativo")
+	}
+
+	if num < 2 {
+		return false, nil
+	}
+
+	limite := int(math.Sqrt(float64(num)))
+
+	for i := 2; i <= limite; i++ {
+		if num%i == 0 {
+			return false, nil
+		}
+	}
+
+	return true, nil
+}
 
